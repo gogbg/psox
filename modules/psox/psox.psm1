c321311aac0cd9920d2cb4bb1faa14d2563a3be9
Georgi Ivanov <gogbg@outlook.com>
2021-03-27 01:54:48 +0200

    wip

---
+++ modules/psox/psox.psm1
@@ -0,0 +1,225 @@
+class psoxTaskResult
+{
+    [bool]$RebootRequired
+    [psobject]$Output
+}
+
+class psoxTask
+{
+    [string]$Name
+    [string]$Module
+    [string]$Version
+    [psoxTaskResult]$result
+    [timespan]$ExecutionTime
+    [psoxTaskState]$State = [psoxTaskState]::Initialized
+}
+
+enum psoxTaskState
+{
+    Initialized
+}
+
+
+function New-PsoxKeyword
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter()]
+        [Management.Automation.Language.DynamicKeywordNameMode]$NameMode = [Management.Automation.Language.DynamicKeywordNameMode]::SimpleNameRequired,
+
+        [Parameter(Mandatory)]
+        [Management.Automation.Language.DynamicKeywordBodyMode]$BodyMode,
+
+        [Parameter()]
+        [scriptblock[]]$SemanticCheck,
+
+        [Parameter()]
+        [System.Management.Automation.Language.DynamicKeywordProperty[]]$Property
+    )
+
+    $psoxKW = [System.Management.Automation.Language.DynamicKeyword]::new()
+    $psoxKW.Keyword = $Name
+    $psoxKW.NameMode = $NameMode
+    $psoxKW.BodyMode = $BodyMode
+    if ($PSBoundParameters.ContainsKey('SemanticCheck'))
+    {
+        foreach ($sc in $SemanticCheck)
+        {
+            $psoxKW.SemanticCheck.Add($sc)
+        }
+    }
+    if ($PSBoundParameters.ContainsKey('Property'))
+    {
+        foreach ($p in $Property)
+        {
+            $psoxKW.Properties.Add($p.Name, $p)
+        }
+    }
+    [System.Management.Automation.Language.DynamicKeyword]::AddKeyword($psoxKW)
+}
+
+#Discover psox tasks
+function Get-PsoxModule
+{
+    [CmdletBinding()]
+    param
+    (
+        [switch]$Refresh
+    )
+
+    if ([datetime]::Now - [psoxModuleCache]::Timestamp -gt [psoxModuleCache]::ExpiryPeriod -or $Refresh.IsPresent)
+    {
+        [psoxModuleCache]::Cache.Clear()
+        Get-Module -Name 'px*' -ListAvailable | ForEach-Object -Process {
+            if ($_.PrivateData.ContainsKey('PsoxData'))
+            {
+                $curMod = [psoxModule]@{
+                    Name    = $_.Name
+                    Version = $_.Version
+                    Tasks   = $_.PrivateData.PsoxData.Tasks
+                }
+                [psoxModuleCache]::Cache.Add($curMod)
+            }
+        }
+        [psoxModuleCache]::Timestamp = [datetime]::Now
+    }
+
+    #return
+    [psoxModuleCache]::Cache
+}
+
+function Test-PsoxTaskDeclaration
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [System.Management.Automation.Language.CommandAst[]]$TaskAst
+    )
+
+    begin
+    {
+        $parseErrors = [System.Collections.Generic.List[System.Management.Automation.Language.ParseError]]::new()
+    }
+    
+    process
+    {
+        foreach ($ta in $TaskAst)
+        {
+            $taskSpec = $pxTaskSpec[$ta.GetCommandName()]
+
+            #Validate Syntax
+            if ($ta.CommandElements[1] -isnot [System.Management.Automation.Language.StringConstantExpressionAst])
+            {
+                $parseErrors.Add([System.Management.Automation.Language.ParseError]::new($ta.Extent, 1, "Missing task name.$([System.Environment]::NewLine)Syntax: $($taskSpec.Syntax)"))
+            }
+            if ($ta.CommandElements[2] -isnot [System.Management.Automation.Language.HashtableAst])
+            {
+                $parseErrors.Add([System.Management.Automation.Language.ParseError]::new($ta.Extent, 1, "Missing task parameters.$([System.Environment]::NewLine)Syntax: $($taskSpec.Syntax)"))
+                throw [System.Management.Automation.ParseException]::new($er)
+            }
+
+            #Validate parameters
+            $mandatoryParamsReq = [System.Collections.Generic.List[string]]::new()
+            foreach ($p in $taskSpec.Parameters.keys)
+            {
+                if ($taskSpec.Parameters[$p].Mandatory)
+                {
+                    $mandatoryParamsReq.Add($p)
+                }
+            }
+            foreach ($kvp in $ta.CommandElements[2].KeyValuePairs)
+            {
+                if ($taskSpec.Parameters.ContainsKey($kvp.Item1.Value))
+                {
+                    if ($kvp.Item2.GetPureExpression().StaticType -ne $taskSpec.Parameters[$kvp.Item1.Value].Type)
+                    {
+                        $parseErrors.Add([System.Management.Automation.Language.ParseError]::new($kvp.Item2.Extent, 1, "Invalid task parameter value type: '$($kvp.Item2.GetPureExpression().StaticType)'. Syntax: $($taskSpec.Syntax)"))
+                    }
+                }
+                else
+                {
+                    $parseErrors.Add([System.Management.Automation.Language.ParseError]::new($kvp.Item1.Extent, 1, "Invalid task parameter name: '$($kvp.Item1.Value)'. Syntax: $($taskSpec.Syntax)"))
+                }
+                if ($taskSpec.Parameters[$kvp.Item1.Value].Mandatory)
+                {
+                    $null = $mandatoryParamsReq.Remove($kvp.Item1.Value)
+                }
+            }
+            if ($mandatoryParamsReq.Count -gt 0)
+            {
+                $parseErrors.Add([System.Management.Automation.Language.ParseError]::new($ta.CommandElements[2].Extent, 1, "Missing mandatory task parameter: '$($mandatoryParamsReq -join ''', ''')'.$([System.Environment]::NewLine)Syntax: $($taskSpec.Syntax)"))
+            }
+        }
+    }
+    end
+    {
+        if ($parseErrors.Count -gt 0)
+        {
+            throw [System.Management.Automation.ParseException]::new($parseErrors)
+        }
+    }
+}
+
+function pxScenario
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, Position = 0)]
+        [string]$Name,
+
+        [Parameter(Mandatory, Position = 1)]
+        [scriptblock]$Body
+    )
+
+    Get-AstStatement -Ast $Body.Ast -Type CommandAst | Where-Object -FilterScript { $pxTaskSpec.ContainsKey($_.GetCommandName()) } | Test-PsoxTaskDeclaration
+}
+
+#region task pxDsc
+function pxDsc
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, Position = 0)]
+        [string]$Name,
+
+        [Parameter(Mandatory, Position = 1)]
+        [hashtable]$Body
+    )
+}
+
+$pxDscSpec = @{
+    TaskName   = 'pxDsc'
+    Syntax     = @'
+pxDsc <Name> @{
+    Resource   = <Resource Name>
+    [ Module   = <Module Name> ]
+    Properties = @{}
+}
+'@
+    Parameters = @{
+        Resource   = @{
+            Mandatory = $true
+            Type      = [string]
+        }
+        Module     = @{
+            Mandatory = $false
+            Type      = [string]
+        }
+        Properties = @{
+            Mandatory = $true
+            type      = [hashtable]
+        }
+    }
+}
+#endregion
+
+$pxTaskSpec = @{
+    $pxDscSpec.TaskName = $pxDscSpec
+}
---
+++ tests/integration/dscTask.tests.ps1
@@ -0,0 +1,24 @@
+$repoRootFolder = Split-Path -Path $PSScriptRoot -Parent | Split-Path -Parent
+$modulesFolder = Join-Path -Path $repoRootFolder -ChildPath 'modules'
+$pathSeperator = [System.IO.Path]::PathSeparator
+if ($modulesFolder -notin ($env:PSModulePath -split $pathSeperator))
+{
+    Write-Verbose -Message 'Prepending modulesFolder directory to $env:PSModulePath'
+    $env:PSModulePath = $modulesFolder, $env:PSModulePath -join $pathSeperator
+}
+Import-Module -Name psox -Force
+
+pxScenario blq {
+    pxDsc Task1 @{
+        Resource   = 'WindowsFeature'
+        Properties = @{
+
+        }
+    }
+    pxDsc alabala @{
+        Resource   = 'WindowsFeature'
+        Properties = @{
+
+        }
+    }
+}
